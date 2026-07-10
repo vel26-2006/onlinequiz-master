50514353d8be15f9ef5f83b30f72144a7b9fe554
Thanigaivel <thanigaivelt235@gmail.com>
2026-07-11 00:11:23 +0530

    Restore forms

--- quiz/forms.py
+++ quiz/forms.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import User
 from . import models
 
 class QuestionForm(forms.Form):
+    class ContactusForm(forms.Form):
+    Name = forms.CharField(max_length=30)
+    Email = forms.EmailField()
+    Message = forms.CharField(widget=forms.Textarea)
+
+class TeacherSalaryForm(forms.Form):
+    salary = forms.IntegerField()
+
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = models.Course
+        fields = ['course_name', 'question_number', 'total_marks']
 
     courseID = forms.ModelChoiceField(
         queryset=models.Course.objects.all(),
